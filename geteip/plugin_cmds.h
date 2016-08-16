0c74ff7cb6978f0a8fafdeb8581e7b3df72a19ab
Kenta Ishiguro <guro.12307910@gmail.com>
2016-08-15 19:45:25 -0700

    Remove unused files

--- geteip/plugin_cmds.h
+++
@@ -1,7 +0,0 @@
-{
-        .name           = "geteip",
-        .args_type      = "procname:s?",
-        .mhandler.cmd   = do_monitor_proc,
-        .params         = "[procname]",
-        .help           = "tracking EIP of [procname] as block"
-},
--- hook_read/plugin_cmds.h
+++
@@ -1,7 +0,0 @@
-{
-        .name           = "hook_read",
-        .args_type      = "procname:s?",
-        .mhandler.cmd   = do_monitor_proc,
-        .params         = "[procname]",
-        .help           = "tracking EIP of [procname] as block"
-},
